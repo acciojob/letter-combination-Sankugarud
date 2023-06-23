004d5afcb1fd3e18d21918fa34d73952a0092000
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-24 00:45:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+
+   const result = [];
+
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+    } else {
+      const currentDigit = nextDigits[0];
+      const letters = digitToLetters[currentDigit];
+
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        backtrack(combination + letter, nextDigits.slice(1));
+      }
+    }
+  }
+
+  backtrack('', digits);
+
+  return result;
 }
+
 
 module.exports = letterCombinations;
